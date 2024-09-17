e0427e854619d5e42f39766254739f266848a6fc
Almaas Izdihar <almaasizdihar@gmail.com>
2024-09-17 18:18:35 +0700

    change qr library and update ui scan-qr page

--- app/tree/scan-qr/page.tsx
+++ app/tree/scan-qr/page.tsx
@@ -1,47 +1,52 @@
+// "use client";
+
 "use client";
 
-import React, { useState } from "react";
-import { QrReader } from "react-qr-reader";
+import { useState, useEffect } from "react";
+import { useZxing } from "react-zxing";
+
 import Image from "next/image";
 import QRPictogram from "@/app/images/qr-pictogram.png";
+import { useRouter } from "next/navigation";
+import BackButton from "@/app/components/BackButton";
 
 const ScanQRPage = () => {
-  const [data, setData] = useState("No result");
+  const router = useRouter();
+
+  const [result, setResult] = useState("");
+  const { ref } = useZxing({
+    onDecodeResult(result) {
+      setResult(result.getText());
+    },
+  });
+
+  useEffect(() => {
+    if (result.trim() !== "") {
+      console.log(result);
+      // alert(result);
+      router.push(`/tree/${result}`)
+    }
+  }, [result, router]);
+
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-devh bg-gray-200">
-      <div className="bg-white py-10 w-full h-full">
-        <div className="relative aspect-square w-full mb-8">
-          <div className="relative bg-gray-200">
-            <QrReader
-              onResult={(result, error) => {
-                if (!!result) {
-                  setData(result?.getText());
-                }
+    <div className="flex flex-col items-center justify-center h-dvh bg-gray-200">
+      <BackButton />
 
-                if (!!error) {
-                  console.info(error);
-                }
-              }}
-              constraints={{ facingMode: "environment" }}
-              containerStyle={{ width: "100%", height: "100%" }}
-              videoContainerStyle={{
-                width: "100%",
-                height: "100%",
-                borderRadius: "1rem",
-                overflow: "hidden",
-              }}
-            />
+      <div className="flex-1 bg-white w-full h-full">
+        <div className="relative h-4/6 w-full mb-8">
+          <div className="relative bg-gray-200 h-full w-full ">
+            <video ref={ref} className="h-full w-full object-cover" />
           </div>
-          <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
-            <div className="w-4/6 h-4/6 border-4 border-white rounded-3xl"></div>
+          <div className="absolute w-full h-full p-5 inset-0 grid place-items-center pointer-events-none">
+            <div className="aspect-square w-full max-h-96 max-w-96 border-4 border-white rounded-3xl"></div>
           </div>
         </div>
 
-        <div className="text-center">
-          <p className="text-lg font-semibold mb-2">Scan QR Code</p>
+        <div className="text-center flex flex-col items-center">
+          <p className="text-lg font-semibold">Scan QR Code</p>
           <p className="text-sm text-gray-600 mb-4">pada Pohon</p>
-          <div className="inline-block rounded-xl">
+          <div className="block rounded-xl">
             <Image
               src={QRPictogram} // Replace with your actual QR code icon
               alt="QR Code Icon"
@@ -49,9 +54,9 @@ const ScanQRPage = () => {
               height={64}
             />
           </div>
-        </div>
-        <div className="overflow-hidden">
-          <p className="mt-4 text-center text-sm ">{data}</p>
+          <span className="text-sm text-gray-600 overflow-hidden mt-4">
+            {result}
+          </span>
         </div>
       </div>
     </div>
