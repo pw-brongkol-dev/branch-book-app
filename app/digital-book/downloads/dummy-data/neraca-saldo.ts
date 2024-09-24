9e4f5128c5c67c3dcfc3fecb5dcef411227f2364
Almaas Izdihar <almaasizdihar@gmail.com>
2024-09-24 22:22:39 +0700

    add dummy data for 3 report

---
+++ app/digital-book/downloads/dummy-data/neraca-saldo.ts
@@ -0,0 +1,146 @@
+const accounts = [
+    {
+      NomorAkun: "1-0000",
+      NamaAkun: "Aktiva",
+      Debit: 0,
+      Kredit: 0
+    },
+    {
+      NomorAkun: "1-1100",
+      NamaAkun: "Kas",
+      Debit: 24800000,
+      Kredit: 0
+    },
+    {
+      NomorAkun: "1-1200",
+      NamaAkun: "Piutang",
+      Debit: 0,
+      Kredit: 0
+    },
+    {
+      NomorAkun: "1-1300",
+      NamaAkun: "Perlengkapan",
+      Debit: 150000,
+      Kredit: 0
+    },
+    {
+      NomorAkun: "1-1400",
+      NamaAkun: "Persediaan Aset Biologis",
+      Debit: 0,
+      Kredit: 0
+    },
+    {
+      NomorAkun: "1-2100",
+      NamaAkun: "Lahan Pertanian",
+      Debit: 50000000,
+      Kredit: 0
+    },
+    {
+      NomorAkun: "1-2200",
+      NamaAkun: "Peralatan",
+      Debit: 0,
+      Kredit: 0
+    },
+    {
+      NomorAkun: "1-2210",
+      NamaAkun: "Akumulasi Penyusutan Peralatan",
+      Debit: 0,
+      Kredit: 0
+    },
+    {
+      NomorAkun: "2-0000",
+      NamaAkun: "Kewajiban",
+      Debit: 0,
+      Kredit: 0
+    },
+    {
+      NomorAkun: "2-1100",
+      NamaAkun: "Utang Usaha",
+      Debit: 0,
+      Kredit: 0
+    },
+    {
+      NomorAkun: "2-2100",
+      NamaAkun: "Utang Jangka Panjang",
+      Debit: 0,
+      Kredit: 0
+    },
+    {
+      NomorAkun: "3-0000",
+      NamaAkun: "Ekuitas",
+      Debit: 0,
+      Kredit: 0
+    },
+    {
+      NomorAkun: "3-1100",
+      NamaAkun: "Modal",
+      Debit: 0,
+      Kredit: 69000000
+    },
+    {
+      NomorAkun: "3-1200",
+      NamaAkun: "Laba",
+      Debit: 0,
+      Kredit: 0
+    },
+    {
+      NomorAkun: "4-0000",
+      NamaAkun: "Pendapatan",
+      Debit: 0,
+      Kredit: 0
+    },
+    {
+      NomorAkun: "4-1100",
+      NamaAkun: "Penjualan Biji Kopi",
+      Debit: 0,
+      Kredit: 11500000
+    },
+    {
+      NomorAkun: "4-1200",
+      NamaAkun: "Retur Penjualan",
+      Debit: 0,
+      Kredit: 0
+    },
+    {
+      NomorAkun: "6-0000",
+      NamaAkun: "Beban-Beban Usaha",
+      Debit: 0,
+      Kredit: 0
+    },
+    {
+      NomorAkun: "6-1100",
+      NamaAkun: "Beban Pupuk",
+      Debit: 200000,
+      Kredit: 0
+    },
+    {
+      NomorAkun: "6-1200",
+      NamaAkun: "Beban Benih",
+      Debit: 4000000,
+      Kredit: 0
+    },
+    {
+      NomorAkun: "6-1300",
+      NamaAkun: "Beban Obat Hama",
+      Debit: 100000,
+      Kredit: 0
+    },
+    {
+      NomorAkun: "6-1400",
+      NamaAkun: "Beban Gaji dan Upah",
+      Debit: 1000000,
+      Kredit: 0
+    },
+    {
+      NomorAkun: "6-1500",
+      NamaAkun: "Beban Transportasi",
+      Debit: 100000,
+      Kredit: 0
+    },
+    {
+      NomorAkun: "6-1600",
+      NamaAkun: "Beban Telp, Listrik, dan Air",
+      Debit: 150000,
+      Kredit: 0
+    }
+  ];
