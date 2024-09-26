a88162ab14a67b7de7fe208d4a63aae5a126051d
Almaas Izdihar <almaasizdihar@gmail.com>
2024-09-26 10:12:23 +0700

    update interface and useFirestore

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,113 +1,205 @@
 'use client';
 
-import { generateDummyData } from './input_db/insertDummyData2';
+import {
+  group_all,
+  user_all,
+  tree_all,
+  fertilization_all,
+  account_all,
+  transaction_all,
+} from './input_db/insertDummyData3';
 import { useFirestore } from '../hooks/useFirestore';
 
 export default function Test() {
-  const { addGroup, addUser, addAccount, addTree, addFertilization, addTransaction, addRelTreeFertilization, deleteAccount, deleteFertilization, deleteGroup, deleteRelTreeFertilization, deleteTransaction, deleteTree, deleteUser } = useFirestore();
+  const {
+    getAllTrees,
+    getAllFertilizations,
+    addGroup,
+    addUser,
+    addAccount,
+    addTree,
+    addFertilization,
+    addTransaction,
+    addRelTreeFertilization,
+    deleteAccount,
+    deleteFertilization,
+    deleteGroup,
+    deleteRelTreeFertilization,
+    deleteTransaction,
+    deleteTree,
+    deleteUser,
+  } = useFirestore();
 
-  const insertDummyData = async () => {
+  async function handleInsertDummyData() {
     try {
-      const dummyData = generateDummyData();
-
       console.log('Inserting dummy data...');
 
-      for (const group of dummyData.group) {
-        await addGroup(group);
-        console.log('Group added:', group.name);
-      }
+      // // Insert groups
+      // const groupIds = await Promise.all(
+      //   group_all.map(async (group) => {
+      //     const id = await addGroup(group);
+      //     return { ...group, id };
+      //   }),
+      // );
+      // console.log(groupIds);
 
-      for (const user of dummyData.user) {
-        await addUser(user);
-        console.log('User added:', user.name);
-      }
+      // // Insert users with group_id
+      // const userIds = await Promise.all(
+      //   user_all.map(async (user, index) => {
+      //     // const id = await addUser({ ...user, group_id: groupIds[index].id });
+      //     const id = await addUser({
+      //       ...user,
+      //       group_id: 'zMeAxIZm2r7ZwAx5fQ89',
+      //     });
+      //     return { ...user, id };
+      //   }),
+      // );
+      // console.log(userIds);
 
-      for (const account of dummyData.account) {
-        await addAccount(account);
-        console.log('Account added:', account.name);
-      }
+      // // Insert trees with user_id
+      // const treeIds = await Promise.all(
+      //   tree_all.map(async (tree, index) => {
+      //     // const id = await addTree({ ...tree, user_id: userIds[index].id });
+      //     const id = await addTree({
+      //       ...tree,
+      //       user_id: 'YPSU116MaRDOYTQQmScT',
+      //     });
+      //     return { ...tree, id };
+      //   }),
+      // );
+      // console.log(treeIds);
 
-      for (const tree of dummyData.tree) {
-        await addTree(tree);
-        console.log('Tree added:', tree.code);
-      }
+      // // Insert fertilizations
+      // const fertilizationIds = await Promise.all(
+      //   fertilization_all.map(async (fertilization) => {
+      //     const id = await addFertilization(fertilization);
+      //     return { ...fertilization, id };
+      //   }),
+      // );
+      // console.log(fertilizationIds);
 
-      for (const fertilization of dummyData.fertilization) {
-        await addFertilization(fertilization);
-        console.log('Fertilization added:', fertilization.title);
-      }
+      // // Insert rel_tree_fertilizations with tree_id and fertilization_id (wrong logic)
+      // const relTreeFertilizationIds = await Promise.all(
+      //   rel_tree_fertilization_all.map(async (rel, index) => {
+      //     const id = await addRelTreeFertilization({ ...rel, tree_id: treeIds[index].id, fertilization_id: fertilizationIds[index].id });
+      //     return { ...rel, id };
+      //   }),
+      // );
+      // console.log(relTreeFertilizationIds);
 
-      for (const transaction of dummyData.transaction) {
-        await addTransaction(transaction);
-        console.log('Transaction added:', transaction.description);
-      }
+      // // Insert accounts
+      // const accountIds = await Promise.all(
+      //   account_all.map(async (account) => {
+      //     const id = await addAccount(account);
+      //     return { ...account, id };
+      //   }),
+      // );
+      // console.log(accountIds);
 
-      for (const rel_tree_fertilization of dummyData.rel_tree_fertilization) {
-        await addRelTreeFertilization(rel_tree_fertilization);
-        console.log('Rel Tree Fertilization added:', rel_tree_fertilization.id);
-      }
+      // // Insert transactions with account_id
+      // const transactionIds = await Promise.all(
+      //   transaction_all.map(async (transaction) => {
+      //     const id = await addTransaction({ ...transaction });
+      //     return { ...transaction, id };
+      //   }),
+      // );
+      // console.log(transactionIds);
 
       console.log('All dummy data inserted successfully!');
     } catch (error) {
       console.error('Error inserting dummy data:', error);
     }
-  };
+  }
 
-  const deleteDummyData = async () => {
+  async function insertRelTreeFertilizationPair() {
     try {
-      const dummyData = generateDummyData();
+      console.log('Generating relationships between trees and fertilizations...');
 
-      console.log('Deleting dummy data...');
+      // Fetch all trees and fertilizations using useFirestore
+      const trees = await getAllTrees(); // Fetch all trees
+      const fertilizations = await getAllFertilizations(); // Fetch all fertilizations
 
-      for (const group of dummyData.group) {
-        await deleteGroup(group.id); // Assuming each group has an 'id'
-        console.log('Group deleted:', group.name);
-      }
+      // Create pairs and save to the database
+      const relTreeFertilizationIds = await Promise.all(
+        trees.map(async (tree) => {
+          fertilizations.map(async (fertilization) => {
+            // Randomly select a fertilization for each tree
+            const rel = {
+              tree_id: tree.id, // Assuming tree has an 'id' property
+              fertilization_id: fertilization.id, // Assuming fertilization has an 'id' property
+              is_completed: false, // Set default value
+            };
+            // DANGER! THIS WILL BE INSERT TO DB
+            // const id = await addRelTreeFertilization(rel);
+            const id = null
+            console.log(rel)
+            return { ...rel, id };
+          })
+        })
+      );
 
-      for (const user of dummyData.user) {
-        await deleteUser(user.id); // Assuming each user has an 'id'
-        console.log('User deleted:', user.name);
-      }
-
-      for (const account of dummyData.account) {
-        await deleteAccount(account.id); // Assuming each account has an 'id'
-        console.log('Account deleted:', account.name);
-      }
-
-      for (const tree of dummyData.tree) {
-        await deleteTree(tree.id); // Assuming each tree has an 'id'
-        console.log('Tree deleted:', tree.code);
-      }
-
-      for (const fertilization of dummyData.fertilization) {
-        await deleteFertilization(fertilization.id); // Assuming each fertilization has an 'id'
-        console.log('Fertilization deleted:', fertilization.title);
-      }
-
-      for (const transaction of dummyData.transaction) {
-        await deleteTransaction(transaction.id); // Assuming each transaction has an 'id'
-        console.log('Transaction deleted:', transaction.description);
-      }
-
-      console.log('All dummy data deleted successfully!');
+      console.log('Generated relationships:', relTreeFertilizationIds);
     } catch (error) {
-      console.error('Error deleting dummy data:', error);
+      console.error('Error generating relationships:', error);
     }
-  };
-
-  function handleInsertDummyData() {
-    insertDummyData();
   }
 
   function handleDeleteDummyData() {
-    deleteDummyData();
+    // try {
+    //   const dummyData = {
+    //     group: group_all,
+    //     user: user_all,
+    //     tree: tree_all,
+    //     fertilization: fertilization_all,
+    //     rel_tree_fertilization: rel_tree_fertilization_all,
+    //     account: account_all,
+    //     transaction: transaction_all,
+    //   };
+    //   console.log("Deleting dummy data...");
+    //   for (const group of dummyData.group) {
+    //     await deleteGroup(group.id); // Assuming each group has an 'id'
+    //     console.log("Group deleted:", group.name);
+    //   }
+    //   for (const user of dummyData.user) {
+    //     await deleteUser(user.id); // Assuming each user has an 'id'
+    //     console.log("User deleted:", user.name);
+    //   }
+    //   for (const account of dummyData.account) {
+    //     await deleteAccount(account.id); // Assuming each account has an 'id'
+    //     console.log("Account deleted:", account.name);
+    //   }
+    //   for (const tree of dummyData.tree) {
+    //     await deleteTree(tree.id); // Assuming each tree has an 'id'
+    //     console.log("Tree deleted:", tree.code);
+    //   }
+    //   for (const fertilization of dummyData.fertilization) {
+    //     await deleteFertilization(fertilization.id); // Assuming each fertilization has an 'id'
+    //     console.log("Fertilization deleted:", fertilization.title);
+    //   }
+    //   for (const transaction of dummyData.transaction) {
+    //     await deleteTransaction(transaction.id); // Assuming each transaction has an 'id'
+    //     console.log("Transaction deleted:", transaction.description);
+    //   }
+    //   console.log("All dummy data deleted successfully!");
+    // } catch (error) {
+    //   console.error("Error deleting dummy data:", error);
+    // }
   }
   return (
     <>
-      <button onClick={handleInsertDummyData}>Insert Dummy Data</button>
+      <button className="border border-black" onClick={handleInsertDummyData}>
+        Insert Dummy Data
+      </button>
       <br />
       <br />
-      <button onClick={handleDeleteDummyData}>Delete Dummy Data</button>
+      <button className="border border-black" onClick={handleDeleteDummyData}>
+        Delete Dummy Data
+      </button>
+      <br />
+      <br />
+      <button className="border border-black" onClick={insertRelTreeFertilizationPair}>
+        Generate Pair Tree-Fertilization
+      </button>
     </>
   );
 }
