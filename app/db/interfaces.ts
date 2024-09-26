0c9b4bab80183164978b1aed3a856d07fa649e9f
uvenven <fadilefd1102@gmail.com>
2024-09-26 21:23:57 +0700

    feat: implement nota input to send data to the database

--- app/db/interfaces.ts
+++ app/db/interfaces.ts
@@ -1,18 +1,18 @@
-import { Timestamp } from "firebase/firestore";
+import { Timestamp } from 'firebase/firestore';
 
 export interface Group {
-  // id: string; 
+  // id: string;
   name: string;
 }
 
 export interface User {
-  // id: string; 
+  // id: string;
   name: string;
   group_id: string;
 }
 
 export interface Tree {
-  // id: string; 
+  // id: string;
   code: string;
   type: string;
   accession: string;
@@ -22,21 +22,21 @@ export interface Tree {
 }
 
 export interface RelTreeFertilization {
-  // id: string; 
+  // id: string;
   tree_id: string;
   fertilization_id: string;
   is_completed: boolean;
 }
 
 export interface Fertilization {
-  // id: string; 
+  // id: string;
   date: Timestamp;
   title: string;
   description: string;
 }
 
 export interface Account {
-  // id: string; 
+  id: string;
   code: string;
   name: string;
   type: string;
@@ -44,7 +44,7 @@ export interface Account {
 }
 
 export interface Transaction {
-  // id: string; 
+  // id: string;
   account_id: string; // Foreign key to Account
   user_id: string; // Foreign key to User
   date: Timestamp;
@@ -52,7 +52,6 @@ export interface Transaction {
   total_amount: number;
   ref: string; // Assuming this is a reference number or code
 }
-
 
 export type GroupWithId = Group & { id: string };
 export type UserWithId = User & { id: string };
