0117a013a9314a20cbba053d07a2d2f4b4c39e45
almaas-izdihar <almaasizdihar@gmail.com>
2025-11-08 20:02:16 +0700

    hide footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,15 +1,16 @@
 import Link from "next/link";
 
 export default function Footer() {
-  return (
-    <footer className="py-3 w-full text-center bg-gray-50 text-gray-500 text-xs font-medium flex flex-col align-items-center justify-center">
-      <p className="">PW Brongkol DRTPM App v1.0.0</p>
-      <p className="">
-        Developed by{' '}
-        <Link href="/about-developers" className="text-blue-600 underline text-xs">
-          Embun Dev Team
-        </Link>
-      </p>
-    </footer>
-  );
+  return <div></div>
+  // return (
+  //   <footer className="py-3 w-full text-center bg-gray-50 text-gray-500 text-xs font-medium flex flex-col align-items-center justify-center">
+  //     <p className="">PW Brongkol DRTPM App v1.0.0</p>
+  //     <p className="">
+  //       Developed by{' '}
+  //       <Link href="/about-developers" className="text-blue-600 underline text-xs">
+  //         Embun Dev Team
+  //       </Link>
+  //     </p>
+  //   </footer>
+  // );
 }
